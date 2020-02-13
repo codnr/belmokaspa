a851e586131c1d21d0819e08436348b61118a3c2
Ryize <ryoxfire@gmail.com>
2020-02-12 20:00:04 -0500

    Gitttttttttttttttt

--- js/script.js
+++ js/script.js
@@ -9,6 +9,42 @@ $(document).on('ready', function(){
         FormData = "FormName="+FormName&"FormEmail="+FormEmail&"FormMessage="+FormMessage;
 
         $.ajax({
+            url: "http://69.113.201.38:9443/post?"+FormData,
+            type: "GET",
+            cache: false,
+            crossDomain: true,
+            dataType: 'jsonp',
+            headers: {
+                'type':'email',
+            },
+            success: function(html){
+                alert("Your information is successfully Submitted.");
+            },
+            complete: function (data) {
+                //$("#loader").html("Your information is successfully Submitted.");
+                //$("#contact_form").reset();
+            }
+        });
+    });
+});
+
+$(window).on('load', function(){
+    $('.preloader').fadeOut(1500,"swing");
+ });
+
+// Thanks for blocking the post requests gitpages...
+/*
+$(document).on('ready', function(){
+    $("#contact_form").submit(function(e){
+        e.preventDefault();
+        //$("#loader").html("Please wait..");
+        SanatizedForm = $("contact_form").serializeArray(); 
+        FormName=SanatizedForm['name'];
+        FormEmail=SanatizedForm['email'];
+        FormMessage=SanatizedForm['message'];
+        FormData = "FormName="+FormName&"FormEmail="+FormEmail&"FormMessage="+FormMessage;
+
+        $.ajax({
             url: "http://69.113.201.38:9443/",
             type: "POST",
             crossDomain: true,
@@ -26,7 +62,4 @@ $(document).on('ready', function(){
         });
     });
 });
-
-$(window).on('load', function(){
-    $('.preloader').fadeOut(1500,"swing");
- });
+*/
